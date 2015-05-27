b8033fb2f92b49bde7091dad5aade75839e7cda7
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-27 16:09:03 -0500

    updates

---
+++ test/index.js
@@ -0,0 +1,20 @@
+var test = require("tape"),
+    isArguments = require("../src");
+
+
+test("isArguments(object): should return true when the value is arguments", function(assert) {
+    assert.equal(isArguments(null), false);
+    assert.equal(isArguments(undefined), false);
+    assert.equal(isArguments({}), false);
+    assert.equal(isArguments({
+        length: 0
+    }), false);
+    assert.equal(isArguments([]), false);
+    assert.equal(isArguments(""), false);
+    assert.equal(isArguments(/./), false);
+    assert.equal(isArguments(function noop() {}), false);
+
+    assert.equal(isArguments(arguments), true);
+
+    assert.end();
+});
