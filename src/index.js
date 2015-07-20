e60274f21194e9be44be723daf2ab7f648ce31d9
nathanfaucett <nathanfaucett@gmail.com>
2015-07-20 08:57:20 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-var isNullOrUndefined = require("is_null_or_undefined");
+var isLength = require("is_length"),
+    isFunction = require("is_function"),
+    isObject = require("is_object"),
+    isNullOrUndefined = require("is_null_or_undefined");
 
 
 var objectToString = Object.prototype.toString,
     isArguments;
 
 
-if ((function() {
+if (
+    (function() {
         return objectToString.call(arguments) === "[object Arguments]";
-    }())) {
+    }())
+) {
     isArguments = function isArguments(value) {
         return !isNullOrUndefined(value) && objectToString.call(value) === "[object Arguments]" || false;
     };
 } else {
     isArguments = function isArguments(value) {
-        return !isNullOrUndefined(value) && !isNullOrUndefined(value.callee) || false;
+        return (
+            isObject(value) &&
+            isFunction(value.callee) &&
+            isLength(value.length)
+        ) || false;
     };
 }
 
