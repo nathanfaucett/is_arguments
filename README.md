66e7dc1b0fedacfd2cbfd1c2e9fc01c401b80805
nathanfaucett <nathanfaucett@gmail.com>
2016-07-13 19:35:12 -0500

    added npm scope, clean up

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 isArguments [![Build Status](https://travis-ci.org/nathanfaucett/is_arguments.svg?branch=master)](https://travis-ci.org/nathanfaucett/is_arguments)
 =======
 
-isArguments for the browser and node.js
+checks if value is an arguments object
+
+```javascript
+var isArguments = require("@nathanfaucett/is_arguments");
+
+
+isArguments([]); // false
+(function() {
+    isArguments(arguments); // true
+}());
+```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-var isLength = require("is_length"),
-    isFunction = require("is_function"),
-    isArray = require("is_array"),
-    isObject = require("is_object"),
-    isNullOrUndefined = require("is_null_or_undefined");
+var isLength = require("@nathanfaucett/is_length"),
+    isFunction = require("@nathanfaucett/is_function"),
+    isArray = require("@nathanfaucett/is_array"),
+    isObject = require("@nathanfaucett/is_object"),
+    isNullOrUndefined = require("@nathanfaucett/is_null_or_undefined");
 
 
 var objectToString = Object.prototype.toString,
